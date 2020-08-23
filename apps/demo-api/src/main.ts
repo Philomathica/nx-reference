a8ef199483b5390b2d392efe2b5b8343ed854959
Luuk Moret <luuk@moretholding.com>
2020-08-23 14:21:21 +0200

    feat(demo-api): add timeout interceptor

--- apps/demo-api/src/main.ts
+++ apps/demo-api/src/main.ts
@@ -7,7 +7,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { HttpExceptionFilter } from '@nx-reference/shared/api';
+import { HttpExceptionFilter, TimeoutInterceptor } from '@nx-reference/shared/api';
 
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
@@ -32,6 +32,7 @@ async function bootstrap() {
   );
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TimeoutInterceptor());
 
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
--- libs/shared/api/src/index.ts
+++ libs/shared/api/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/shared-api.module';
 export * from './lib/filters/http-exception.filter';
 export * from './lib/dto/pagination-query.dto';
+export * from './lib/interceptors/timeout.interceptor';
