c90b71a077832703110fb4bbfc195b1e788cc055
Luuk Moret <luuk@moretholding.com>
2020-09-16 16:12:03 +0200

    ci: #7 add github workflows

--- apps/demo-angular-web-e2e/src/integration/app.spec.ts
+++ apps/demo-angular-web-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('demo-angular-web', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to demo-angular-web!');
+    getGreeting().contains('Sensors');
   });
 });
