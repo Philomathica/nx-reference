eca795a680bf2df0d09423150ad18142e18c135a
Luuk Moret <luuk@moretholding.com>
2020-10-17 09:23:37 +0200

    feat(packages): upgrade packages

--- libs/shared/util/typescript/jest.config.js
+++ libs/shared/util/typescript/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  name: 'shared-util-typescript',
-  preset: '../../../../jest.config.js',
+  preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
@@ -12,4 +11,5 @@ module.exports = {
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../../../coverage/libs/shared/util/typescript',
+  displayName: 'shared-util-typescript',
 };
